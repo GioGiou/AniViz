ed0ada05f938b221c12b96f2079d7c58ee0b940a
GioGiou <GioGiou@users.noreply.github.com>
2024-08-11 20:56:40 +0200

    Table

---
+++ src/routes/genre/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { onMount } from "svelte"
+    import PapaParse from 'papaparse';
+
+    import { base } from '$app/paths'
+	import * as d3 from "d3"
+    import { DataHandler, Datatable, Th, ThFilter } from '@vincjo/datatables'
+    import fs from 'fs';
+    /**@type String*/
+    let data;
+    /**@type any*/
+    let handler;
+    let rows;
+    let dataT;
+    onMount(async ()=>{
+        const rez = await fetch("/animes.csv");
+        data  = await rez.text();
+        dataT= PapaParse.parse(data,{header:true});
+    });
+</script>
+<!-- svelte-ignore event_directive_deprecated -->
+
+
+
+
+        
+
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
 import { enhancedImages } from '@sveltejs/enhanced-img';
-
+import dsv from '@rollup/plugin-dsv' 
 export default defineConfig({
-	plugins: [enhancedImages(),		sveltekit()]
+	plugins: [enhancedImages(),		sveltekit(), dsv()],
+	server: {
+		fs: {
+		  allow: [
+			// search up for workspace root
+			searchForWorkspaceRoot(process.cwd()),
+			// your custom rules
+			'./src/*',
+			'./static/'
+		  ],
+		},
+	  }
 });
